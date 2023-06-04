5feba44cfb99c532c34f44eef9272b1bca152927
Léo Guillaume <leo.guillaume@gmail.com>
2023-06-04 23:58:55 +0200

    feat: update title for test ci

--- components/sections/Landing.vue
+++ components/sections/Landing.vue
@@ -12,7 +12,7 @@
         </div>
         <div class="text-center">
           <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Data to enrich your online business
+            Dibodev
           </h1>
           <p class="mt-6 text-lg leading-8 text-gray-600">
             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
