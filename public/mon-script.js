701091f59885f9d833e60a4e1f4aa6d17ef90fbe
Léo Guillaume <leo.guillaume@epitech.eu>
2024-02-18 04:49:56 +0100

    feat: test beforeunload compatibility

--- public/mon-script.js
+++ public/mon-script.js
@@ -5,17 +5,22 @@
 //     confirmFirst('beforeunload')
 // })
 
-window.addEventListener('pagehide', () => {
-    confirmFirst('pagehide')
+// window.addEventListener('pagehide', () => {
+//     confirmFirst('pagehide')
+//
+// })
+//
+// confirm('v4')
+//
+// function confirmFirst(eventName) {
+//   if(confirm("Leave the page?" + eventName)){
+//     console.log("You are leaving the page")
+//   }else{
+//     return false;
+//   }
+// }
 
-})
 
-confirm('v4')
-
-function confirmFirst(eventName) {
-  if(confirm("Leave the page?" + eventName)){
-    console.log("You are leaving the page")
-  }else{
-    return false;
-  }
-}
+document.addEventListener("visibilitychange", () => {
+  alert(document.hidden)
+});
